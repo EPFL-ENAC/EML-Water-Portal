6e73946aa3cf1df3fec91eee44297e436adaf653
ymarcon <yannick.marcon@epfl.ch>
2024-09-10 09:45:40 +0200

    feat: format sensor dialog

--- src/components/SensorDialog.vue
+++ src/components/SensorDialog.vue
@@ -6,9 +6,47 @@
       </q-card-actions>
       <q-card-section>
         <div class="text-h6 q-mb-sm">
+          {{ `Sensor: ${sensorProperties?.name}` }}
         </div>
         <div>
-          <pre>{{ sensorProperties }}</pre>
+          <q-list v-if="sensorProperties" separator>
+            <q-item>
+              <q-item-section>
+                <q-item-label header class="text-overline">Location name</q-item-label>
+              </q-item-section>
+              <q-item-section>
+                <span>{{ sensorProperties.locationName }}</span>
+              </q-item-section>
+            </q-item>
+            <q-item>
+              <q-item-section>
+                <q-item-label header class="text-overline">Device</q-item-label>
+              </q-item-section>
+              <q-item-section>
+                <span>{{ sensorProperties.sensor }}</span>
+              </q-item-section>
+            </q-item>
+            <q-item>
+              <q-item-section>
+                <q-item-label header class="text-overline">Parameters</q-item-label>
+              </q-item-section>
+              <q-item-section>
+                <ul class="q-pl-md">
+                <template v-for="measure in sensorMeasures" :key="measure">
+                  <li>{{ measure }}</li>
+                </template>
+              </ul>
+              </q-item-section>
+            </q-item>
+            <q-item>
+              <q-item-section>
+                <q-item-label header class="text-overline">Color</q-item-label>
+              </q-item-section>
+              <q-item-section>
+                <q-icon name="circle" :style="{ color: sensorProperties.color }" size="sm"/>
+              </q-item-section>
+            </q-item>
+          </q-list>
         </div>
       </q-card-section>
       <q-card-actions v-if="$q.screen.gt.xs" align="right">
@@ -24,6 +62,7 @@ export default defineComponent({
 });
 </script>
 <script setup lang="ts">
+import { MeasureOptions } from 'src/utils/options';
 
 interface Props {
   modelValue: boolean;
@@ -44,6 +83,10 @@ watch(
 );
 
 const sensorProperties = computed(() => mapStore.sensorSelected?.properties);
+
+const sensorMeasures = computed(() => sensorProperties.value?.measures
+  .split('|')
+  .map((m: string) => MeasureOptions.find((mo) => mo.key === m)?.label || m.replaceAll('_',' ')) || []);
 
 function onHide() {
   emit('update:modelValue', false);
